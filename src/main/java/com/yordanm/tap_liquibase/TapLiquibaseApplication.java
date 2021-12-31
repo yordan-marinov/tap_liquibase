81e7419e09ac5867bb56afb68e1d3b97d46399e9
yordan-marinov <jordanmarinov8@gmail.com>
2021-12-31 19:48:34 +0200

    initial commit

---
+++ src/main/java/com/yordanm/tap_liquibase/TapLiquibaseApplication.java
@@ -0,0 +1,13 @@
+package com.yordanm.tap_liquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TapLiquibaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TapLiquibaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yordanm/tap_liquibase/TapLiquibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yordanm.tap_liquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TapLiquibaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
